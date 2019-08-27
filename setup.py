212667b99683688111b67915c2dc400c50e7859c
Jean Giard <jgiard@antidot.net>
2019-08-27 14:50:55 +0200

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='jira-wiki-md',
-      version='0.1',
+      version='0.2',
       author='Jean Giard',
       license='LGPL',
       classifier=[
