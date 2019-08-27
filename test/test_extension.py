3181aae44d6d7faa94274e277aa63b6b28c67036
Jean Giard <jgiard@antidot.net>
2019-08-27 14:45:37 +0200

    Improve code block rendering

--- test/test_extension.py
+++ test/test_extension.py
@@ -10,9 +10,26 @@ def test_monospace():
 
     assert md == "<p>foo <pre>bar</pre> quz</p>"
 
+
 def test_code():
     text = "foo\n{code}\nbar\n{code}"
 
     md = markdown.markdown(text, extensions=[JiraWikiExtension()])
 
     assert md == "<p>foo\n<pre><code>\nbar\n</code></pre>\n</p>"
+
+
+def test_code_with_html():
+    text = "foo\n{code}\nbar<br>\n{code}"
+
+    md = markdown.markdown(text, extensions=[JiraWikiExtension()])
+
+    assert md == "<p>foo\n<pre><code>\nbar&lt;br&gt;\n</code></pre>\n</p>"
+
+
+def test_code_with_markdown():
+    text = "foo\n{code}\n*bar*\n{code}"
+
+    md = markdown.markdown(text, extensions=[JiraWikiExtension()])
+
+    assert md == "<p>foo\n<pre><code>\n*bar*\n</code></pre>\n</p>"
